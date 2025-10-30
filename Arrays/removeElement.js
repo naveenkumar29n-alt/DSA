e086a6ae3aa1ae9b38f42090c6e5a94c786369d3
naveenkumar2929n <naveenkumar2929n@gmail.com>
2025-10-31 02:11:11 +0530

    array questions

---
+++ Arrays/removeElement.js
@@ -0,0 +1,15 @@
+function remove(arr,val){
+  let x = 0;
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]!=val){
+      arr[x]=arr[i];
+      x++;
+    }
+  }
+  return x;
+}
+
+
+let arr=[1,2,3,4,5];
+let ans=remove(arr,3);
+console.log(ans);
